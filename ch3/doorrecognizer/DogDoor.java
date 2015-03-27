715d37f138cc533dac34ce290c0c65ebad8ff536
miguelalba <miguelalba@users.noreply.github.com>
2015-03-28 00:11:04 +0100

    Dog door with bark recognizer

---
+++ ch3/doorrecognizer/DogDoor.java
@@ -0,0 +1,48 @@
+package ch3.doorrecognizer;
+
+public class DogDoor {
+
+	private boolean open;
+
+	public DogDoor() {
+		this.open = false;
+	}
+
+	public void open() {
+		System.out.println("The dog door opens.");
+		open = true;
+	}
+
+	public void close() {
+		System.out.println("The dog door closes.");
+		open = false;
+	}
+
+	public boolean isOpen() {
+		return open;
+	}
+
+	public static void main(String[] args) {
+		DogDoor door = new DogDoor();
+	    BarkRecognizer recognizer = new BarkRecognizer(door);
+	    Remote remote = new Remote(door);
+
+	    // Simulate the hardware hearing a bark
+	    System.out.println("Fido barks to go outside...");
+	    recognizer.recognize("Woof");
+
+	    System.out.println("\nFido has gone outside...");
+	    System.out.println("\nFido's all done...");
+
+	    try {
+	      Thread.currentThread().sleep(10000);
+	    } catch (InterruptedException e) { }
+
+	    System.out.println("...but he's stuck outside!");
+
+	    System.out.println("\nFido starts barking...");
+	    recognizer.recognize("Woof");
+
+	    System.out.println("\nFido's back inside...");
+	}
+}
