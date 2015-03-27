e7ee24bf70baece8874be36858c6937de2d0f811
miguelalba <miguelalba@users.noreply.github.com>
2015-03-27 18:25:56 +0100

    Rick's final application

---
+++ ch1/rickfinal/Guitar.java
@@ -0,0 +1,26 @@
+package ch1.rickfinal;
+
+public class Guitar {
+
+	private String serialNumber;
+	private double price;
+	GuitarSpec spec;
+
+	public Guitar(String serialNumber, double price, GuitarSpec spec) {
+		this.serialNumber = serialNumber;
+		this.price = price;
+		this.spec = spec;
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public GuitarSpec getSpec() {
+		return spec;
+	}
+}
---
+++ ch1/rickfinal/Type.java
@@ -0,0 +1,13 @@
+package ch1.rickfinal;
+
+public enum Type {
+	ACOUSTIC, ELECTRIC;
+
+	public String toString() {
+		switch (this) {
+			case ACOUSTIC:	return "acoustic";
+			case ELECTRIC:	return "electric";
+			default:		return "unspecified";
+		}
+	}
+}
