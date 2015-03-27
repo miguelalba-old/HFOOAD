c9a0288e0607f22899ef83235632a3c2270e4451
miguelalba <miguelalba@users.noreply.github.com>
2015-03-27 17:53:49 +0100

    Search tool with encapsulation

---
+++ ch1/rickencapsulation/Builder.java
@@ -0,0 +1,17 @@
+package ch1.rickencapsulation;
+
+public enum Builder {
+	FENDER, MARTIN, GIBSON, COLLINGS, OLSON, RYAN, PRS, ANY;
+
+	public String toString() {
+		switch (this) {
+			case FENDER:	return "Fender";
+			case MARTIN:	return "Martin";
+			case GIBSON:	return "Gibson";
+			case COLLINGS:	return "Collings";
+			case OLSON:		return "Olson";
+			case RYAN:		return "PRS";
+			default:		return "unspecified";
+		}
+	}
+}
