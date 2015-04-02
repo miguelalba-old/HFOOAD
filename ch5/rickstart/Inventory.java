9b51aa80585819b75572f913939e637c1ec5a7cf
miguelalba <miguelalba@users.noreply.github.com>
2015-04-02 12:41:10 +0200

    CH5: Rick initial search application

---
+++ ch5/rickstart/Inventory.java
@@ -0,0 +1,31 @@
+package ch5.rickstart;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Inventory {
+
+	private List<Guitar> guitars = new LinkedList<>();
+
+	public void addGuitar(String serialNumber, double price, GuitarSpec spec) {
+		guitars.add(new Guitar(serialNumber, price, spec));
+	}
+
+	public Guitar getGuitar(String serialNumber) {
+		for (Guitar guitar : guitars) {
+			if (guitar.getSerialNumber().equals(serialNumber)) {
+				return guitar;
+			}
+		}
+		return null;
+	}
+
+	public List<Guitar> search(GuitarSpec searchSpec) {
+		List<Guitar> matchingGuitars = new LinkedList<>();
+		for (Guitar guitar : guitars) {
+			if (guitar.getSpec().matches(searchSpec))
+				matchingGuitars.add(guitar);
+		}
+		return matchingGuitars;
+	}
+}
